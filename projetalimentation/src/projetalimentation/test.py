2022b7d2e21ff5b189ea7f4ba686286947bd0723
Thomas Barat <thomas.barat@etu.uni-tours.fr>
2025-04-06 12:38:22 +0200

    Premiére ébauche:
    - Lecteure data base
    
    - code d'opti des calorie et autre
    
    - phrase resultats
    
    - rename pour évite les probléme

---
+++ projetalimentation/src/projetalimentation/test.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as so
+from IPython.display import display
+
+np.set_printoptions(precision=1, suppress=True)
+
+
+
+
+
+Al = pd.read_csv(
+                'C:/Cours/Supply Chain/SC/projetalimentation/data/Aliments.csv', 
+                sep=';', 
+                index_col=0)
+
+Al.iloc[[10*k for k in range(5)],:]
+
+
+
+A = np.array(Al).T
+coeff = A[:-1]
+z_ori1 = A[-1]  # Prix
+beta = np.array([75, # Proteines
+                90, # Lipides
+                225, # Glucides
+                2000, # Calories
+                9, # Fer
+                800, # Calcium 
+                45]) # Fibre
+
+Result = so.linprog(z_ori1, A_ub = -coeff, b_ub = -beta, method = 'highs')
+
+
+
+print( "Cout minimal : ", Result.fun)
+print("Quantité optimale : ", Result.x)
+
+
+
+
+u = np.where(Result.x > 0)[0]  # Qté opti sup à 0
+
+for k in u:
+    print(Al.index[k])  # Nom de l'aliment
+    print('{:0.2f} g'.format(Result.x[k] * 100))  # Quantité optimale en grammes
+
+def rename_aliment(name):
+    return name.replace("–", "-").strip()
+
+Phrase = 'Un repas est constitué de '
+for s in range(len(u) - 1):
+    if s > 0:
+        Phrase += ','
+    gr = Result.x[u[s]] * 100  # Quantité optimale en grammes
+    old_name = Al.index[u[s]]  # Nom de l'aliment
+    name = rename_aliment(old_name)  # Renommer l'aliment (fonction existante)
+    Phrase += ' de {:0.2f} g de {}'.format(gr, name)
+
+# Dernier aliment
+s = len(u) - 1
+gr = Result.x[u[s]] * 100
+old_name = Al.index[u[s]]
+name = rename_aliment(old_name)
+Phrase += ' et de {:0.2f} g de {}. '.format(gr, name)
+Phrase += ' Il coûte un total de {:0.2f} euros.'.format(Result.fun)
+
+print(Phrase)
