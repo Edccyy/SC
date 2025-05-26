c8676eb367689fc5334656de94c841455e658d2a
Thomas Barat <thomas.barat@etu.uni-tours.fr>
2025-05-26 15:12:07 +0200

    Ajout de la fonction "ajouter_besoins"

--- projetalimentation/src/projetalimentation/__init__.py
+++ projetalimentation/src/projetalimentation/__init__.py
@@ -3,8 +3,7 @@ from graphlib import *
 
 class besoins(object):
 
-    def __init__(self, nom:str, proteines:int, lipides:int, glucides:int, calories:int, fer:int, calcium, fibres:int, jours:int):
-        self.nom :str = nom
+    def __init__(self, proteines:int, lipides:int, glucides:int, calories:int, fer:int, calcium, fibres:int, jours:int):
         self.proteines:int = proteines*jours
         self.lipides:int = lipides*jours
         self.glucides:int = glucides*jours
@@ -13,15 +12,6 @@ class besoins(object):
         self.calcium:int = calcium*jours
         self.fibres:int = fibres*jours
         self.jours:int = jours
-
-    def __eq__(self, autre):
-        return self.nom == autre.nom
-    
-    def __super__(self, autre):
-        return self.nom > autre.nom
-    
-    def __sub__(self, autre):
-        return self.nom < autre.nom
 
     def __hash__(self):
         return hash(self.nom)
