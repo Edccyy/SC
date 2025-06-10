4a6811c42583a6efea3aa20e35290fdcf6246fab
Thomas Barat <thomas.barat@etu.uni-tours.fr>
2025-06-10 15:19:45 +0200

    J'avais oublier les docstring pour demo.py

--- projetalimentation/demo.py
+++ projetalimentation/demo.py
@@ -1,4 +1,36 @@
 from src.projet_a import *
 
-#data.save()
+"""
+Ce fichier contient le code de base pour résoudre un problème d'optimisation nutritionnelle.
+Il permet de gérer les besoins nutritionnels, d'ajouter des aliments,
+et de calculer la liste d'aliments à consommer pour répondre aux besoins.
+
+Appuyer sur "1" pour changer les besoins nutritionnels,
+"2" pour ajouter un aliment,
+"3" pour calculer la liste d'aliments à consommer,
+et "4" pour quitter l'application.
+
+
+Pour ajouter un aliment, il faut fournir les informations suivantes :
+    - Besoins en protéines
+    - Besoins en lipides
+    - Besoins en glucides
+    - Besoins en calories
+    - Besoins en fer
+    - Besoins en calcium
+    - Besoins en fibres
+    - Prix de l'aliment
+    - Nom de l'aliment
+
+Pour changer les besoins nutritionnels, il faut fournir les informations suivantes :
+    - Besoins en protéines
+    - Besoins en lipides
+    - Besoins en glucides
+    - Besoins en calories
+    - Besoins en fer
+    - Besoins en calcium
+    - Besoins en fibres
+    - Nombre de jours
+"""
+
 app.run()
